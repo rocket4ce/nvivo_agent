0f6db4a4a1a3e896dae01324aceec376e8f7de82
Dinko Azema <rocket4ce@gmail.com>
2025-10-07 18:00:05 -0300

    feat: Add livekitex_agent dependency to project

--- mix.exs
+++ mix.exs
@@ -67,7 +67,8 @@ defmodule NvivoAgent.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:livekitex_agent, github: "rocket4ce/livekitex_agent", branch: "main"}
     ]
   end
 
